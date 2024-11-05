9b78b494dd4723eb550f03f4756e5c50571c5ec4
suvarna1228 <suvu1228@gmail.com>
2024-11-05 15:40:48 +0530

    pichart

---
+++ piechart.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+exp_vals = [1400,600,300,410,250]
+exp_labels = ["Home Rent","Food","Phone/Internet Bill","Car ","Other Utilities"]
+plt.pie(exp_vals,labels=exp_labels)
+plt.show()
