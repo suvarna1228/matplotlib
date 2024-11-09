7adb07f3b6b6f78b4833b63494d4000a66f3b613
suvarna1228 <suvu1228@gmail.com>
2024-11-10 00:17:54 +0530

    basics arrays

---
+++ 1.py
@@ -0,0 +1,20 @@
+import numpy as np
+import math
+
+c= np.empty(2)
+print(c)
+
+a= np.zeros(2)
+print(a)
+
+b= np.ones(2)
+print(b)
+
+d=np.arange(4)
+print(d)
+
+e= np.linspace(0,10,num=5,dtype=int)
+print(e)
+
+f=np.ones(2,dtype=int)
+print(f)
