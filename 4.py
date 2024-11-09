7b9e67f71bddedcfb412b26efa6b26959a18e10e
suvarna1228 <suvu1228@gmail.com>
2024-11-10 00:55:17 +0530

    arrays in numpy

---
+++ 4.py
@@ -0,0 +1,10 @@
+import numpy as np
+import math
+
+a=np.array([1, 2, 3,5])
+print(a[1])
+print(a[0:2])
+print(a[2])
+print(a[1:])
+print(a[3])
+print(a[-2:])
---
+++ 5.py
@@ -0,0 +1,8 @@
+import numpy as np
+import math
+
+a=np.array([[1, 2], [3, 4], [5, 6]])
+print(a)
+print(a.max())
+print(a.min())
+print(a.sum())
